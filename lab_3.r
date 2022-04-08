28a54f4be84e8954145f9f7c00b5c8dc1e22c495
Kirill <72963968+NotOnlyButAnd@users.noreply.github.com>
2022-04-09 00:36:51 +0300

    Lab 3 [4]
    Задание 4 готово. Если необходимо - осталось составить отчет

--- lab_3.r
+++ lab_3.r
@@ -1,6 +1,9 @@
 # выгружаем полученную таблицу в переменную websites
-websites <- read.csv(file = "C:/Users/s0153478/Downloads/websites.csv", header = TRUE, sep = ",", encoding = "UTF-8")
+# websites <- read.csv(file = "C:/Users/s0153478/Downloads/websites.csv", header = TRUE, sep = ",", encoding = "UTF-8")
+websites <- read.csv(file = "F:/Кирилл/003 УНИВЕР/3 курс/004 2 семестр/big data/websites.csv", header = TRUE, sep = ",", encoding = "UTF-8")
 websites
+
+rm(websites.variables)
 
 # выбираем только переменные
 websites.variables <- websites[,3:13]
@@ -57,3 +60,10 @@ boxplot(websites.variables, horizontal = TRUE, las = 1)
 #### СОРТИРОВКА ####
 # сортируем строки датафрейма по столбцу instagramm.com
 websites.sorted <- websites[order(websites$instagramm.com),];websites.sorted
+
+# формируем subdataset (по шкале instagramm.com > 0.7)
+websites.subdata <- websites[websites$instagramm.com > 0.7,]; websites.subdata
+# выбираем только переменные
+websites.subdata.variables <- websites.subdata[,3:13]
+# можем провести тот же анализ, что и до этого, но ограничимся просто боксплотом
+boxplot(websites.subdata.variables, horizontal = TRUE, las = 1, main = "Респонденты, ответ которых по Instagramm > 0.7")
