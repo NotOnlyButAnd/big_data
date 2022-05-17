1154284e336d482cc3a7add07882c976d47f90fa
Kirill <72963968+NotOnlyButAnd@users.noreply.github.com>
2022-05-17 18:54:26 +0300

    Lab 5.1, 5.2  [v0.1]
    Обновил scrapping. Дополнил другими годами, поместил основной код в функцию для многократного вызова и чтения данных о разных годах

--- lab_5.r
+++ lab_5.r
@@ -9,27 +9,40 @@ library("vctrs")
 # на сайте https://www.numbeo.com/quality-of-life/rankings_by_country.jsp?title=2015&region=154
 # есть только United Kingdom и Ireland. Отдельно Англия там не представлена
 
-# читаем html 2014 года
-url = read_html('https://www.numbeo.com/quality-of-life/rankings_by_country.jsp?title=2014')
+# т.к. страницы с уровнем жизни и их URL отличаются только годами в конце URL адреса, было принято решение
+# создать функцию для избежания дублирования кода
+getCountriesIndexesByYear <- function(year){
+  # читаем html года year
+  url = read_html(paste0('https://www.numbeo.com/quality-of-life/rankings_by_country.jsp?title=', toString(year)))
+  
+  # достаем узлы-таблицы. нам нужна с id="t2", т.е. [2]
+  nodes = html_nodes(url, 'table')
+  
+  # закидываем таблицу в датафрейм
+  df = html_table(nodes[2]) %>% as.data.frame()
+  
+  # почему-то не прочитался стробец Rank (заполнился NA), но это по факту номер строки
+  # поэтому его просто убираем
+  df = df[][2:11]
+  
+  # выбираем из датафрейма только те строки, которые нам нужны
+  df_new = rbind(df[df$Country == "Norway", ],
+                 df[df$Country == "Germany", ],
+                 df[df$Country == "United Kingdom", ],
+                 df[df$Country == "Greece", ])
+  # чтобы просто были пронумерованы строки, а не как в изначальном датафрейме по рангам
+  # хотя может это потом и пригодится
+  rownames(df_new) <- c(1,2,3,4)
+  df_new
+}
 
-# достаем узлы-таблицы. нам нужна с id="t2", т.е. [2]
-nodes = html_nodes(url, 'table'); nodes[2]
+# получаем данные в разные датфреймы по 4 странам за 2014-2021 года
+df_2014 = getCountriesIndexesByYear(2014)
+df_2015 = getCountriesIndexesByYear(2015)
+df_2016 = getCountriesIndexesByYear(2016)
+df_2017 = getCountriesIndexesByYear(2017)
+df_2018 = getCountriesIndexesByYear(2018)
+df_2019 = getCountriesIndexesByYear(2019)
+df_2020 = getCountriesIndexesByYear(2020)
+df_2021 = getCountriesIndexesByYear(2021)
 
-# закидываем таблицу в датафрейм
-df_2014 = html_table(nodes[2]) %>% as.data.frame(); df_2014
-
-# почему-то не прочитался стробец Rank (заполнился NA), но это по факту номер строки
-# поэтому его просто убираем
-df_2014 = df_2014[][2:11]
-df_2014
-
-
-# выбираем из датафрейма только те строки, которые нам нужны
-df_2014_new = rbind(df_2014[df_2014$Country == "Norway", ],
-                    df_2014[df_2014$Country == "Germany", ],
-                    df_2014[df_2014$Country == "United Kingdom", ],
-                    df_2014[df_2014$Country == "Greece", ])
-# чтобы просто были пронумерованы строки, а не как в изначальном датафрейме по рангам
-# хотя может это потом и пригодится
-rownames(df_2014_new) <- c(1,2,3,4)
-df_2014_new
